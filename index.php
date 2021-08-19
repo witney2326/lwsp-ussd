1e152a05ac4c97efd4155e6abf21efcc0154d665
witney2326 <wkabango@gmail.com>
2021-08-19 10:35:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,105 @@
 <?php
-// Reads the variables sent via POST
+// variables sent via POST
 $sessionId   = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
-$text = $_POST["text"];
-$hh_code = $_GET["hh_name"];
-$contractor_name = $_POST["contractor_name"];
-$area_name = $_POST["area_name"];
-$phone_number = $_POST["phone_number"];
+$text = $_POST["text"]; 
 
+$response = '';
 
-//This is the first menu screen
-if ($text == ""){
-  $response  = "CON Welcome To OSS Platform \n";
-  $response .= "1. Request for Service \n";
-  $response .= "2. Pay/Contribute for Service \n";
-  $response .= "3. Works Progress \n";
+$textArr = explode('*', $text); 
+$level = count($textArr);
 
+if ( $textArr[0] == null ) {
+
+  // This is the first request. 
+  $response = "CON Welcome to To LWSP OSS Platform \n";
+  $response .= "1. Registration \n";
+  $response .= "2. Request for Service \n";
+  $response .= "3. Pay/Contribute for Service \n";
+  $response .= "4. Works Progress \n";
 }
-elseif ( $text == "1" ) {
-  $response  = "CON  Request For Service \n";
+elseif ($text == "1") {
+  // when user respond with option one to register
+  $response = "CON Choose Registration \n";
+  $response .= "1. Household \n";
+  $response .= "2. Contractor \n";
+  $response .= "3. Supervisor \n";
+  $response .= "4. Ward Committee \n";
+}
+elseif ($text == "1*1") {
+  // registering users
+  $response = "CON Please enter Household name";
+}
+elseif ($textArr[0] == 1 && $textArr[1] == 1 && $level == 3) {
+  $response = "CON Please enter your Area";
+}
+elseif ($textArr[0] == 1 && $textArr[1] == 1 && $level == 4) {
+  $response = "CON Please enter your Block Name";
+}
+elseif ($textArr[0] == 1 && $textArr[1] == 1 && $level == 5) {
+  // save data in the database
+  $response = "END Household data has been captured successfully! Thank you LWSP.";
+}
+
+elseif ($text == "1*2") {
+  // registering contractors
+  $response = "CON Please enter Contractor name";
+}
+elseif ($textArr[0] == 1 && $textArr[1] == 2 && $level == 3) {
+  $response = "CON Please enter  Area of operation";
+}
+elseif ($textArr[0] == 1 && $textArr[1] == 2 && $level == 4) {
+  $response = "CON Please enter your email address";
+}
+elseif ($textArr[0] == 1 && $textArr[1] == 2 && $level == 5) {
+  // save data in the database
+  $response = "END contractor data has been captured successfully! Thank you LWSP.";
+}
+
+elseif ($text == "1*3") {
+  // registering supervisors
+  $response = "CON Please enter Supervisor name";
+}
+elseif ($textArr[0] == 1 && $textArr[1] == 3 && $level == 3) {
+  $response = "CON Please enter  Area of operation";
+}
+elseif ($textArr[0] == 1 && $textArr[1] == 3 && $level == 4) {
+  $response = "CON Please enter your email address";
+}
+elseif ($textArr[0] == 1 && $textArr[1] == 3 && $level == 5) {
+  // save data in the database
+  $response = "END supervisor data has been captured successfully! Thank you LWSP.";
+}
+
+elseif ($text == "1*4") {
+  // registering ward committee
+  $response = "CON Please enter ward committee name";
+}
+elseif ($textArr[0] == 1 && $textArr[1] == 4 && $level == 3) {
+  $response = "CON Please enter  Area of operation";
+}
+elseif ($textArr[0] == 1 && $textArr[1] == 4 && $level == 4) {
+  $response = "CON Please enter your email address";
+}
+elseif ($textArr[0] == 1 && $textArr[1] == 4 && $level == 5) {
+  // save data in the database
+  $response = "END ward commitee data has been captured successfully! Thank you LWSP.";
+}
+
+
+else if ( $text == "2" ) {
+  $response  = "CON Request For Service \n";
   $response .= "1. Technology Choice \n";
   $response .= "2. Technology Cost \n";
-  
 }
 
-else if ( $text == "1*1" ) {
+//else if (array_key_exists(1, $textArr) ) { 
+//	$respon =  "CON Enter Your Household Code \n";
+
+//}
+
+// check here
+else if ( $text == "2*1" ) {
   $response  = "CON  Technology Choice \n";
   $response .= "1. Single VIP Toilet \n";
   $response .= "2. Twin VIP Toilet \n"; 
@@ -32,89 +107,153 @@ else if ( $text == "1*1" ) {
   $response .= "4. Septic Tank \n";
 }
 
-else if ( $text == "1*1*1" ) {
-  $response .= "END  Selected SVIP Toilet \n";
+//else if ($text=2*1*1 ) {
+ // $response = "CON Enter Name For Selected Single VIP Toilet";
+ // $response = "END  Selected SVIP Toilet \n";
+//}
+
+elseif ($textArr[0] == 2 && $textArr[1] == 1 && $textArr[2] == 1 && $level == 3) {
+  $response = "CON Please enter your HH Code";
 }
-else if ( $text == "1*1*2" ) {
-  $response .= "END  Selected TVIP Toilet \n";
-}
-else if ( $text == "1*1*3" ) {
-  $response .= "END  Selected Pour Flash Toilet \n";
-}
-else if ( $text == "1*1*4" ) {
-  $response  = "END  Selected Septic Tank \n";
+elseif ($textArr[0] == 2 && $textArr[1] == 1 && $textArr[2] == 1 && $level == 4) {
+  // save data in the database
+  $response = "END Selected Single VIP Toilet has been captured successfully! Thank you LWSP.";
 }
 
-else if ( $text == "1*2" ) {
+//else if ( $text == "2*1*2" ) {
+  //$response .= "END  Selected TVIP Toilet \n";
+//}
+elseif ($textArr[0] == 2 && $textArr[1] == 1 && $textArr[2] == 2 && $level == 3) {
+  $response = "CON Please enter your HH Code";
+}
+elseif ($textArr[0] == 2 && $textArr[1] == 1 && $textArr[2] == 2 && $level == 4) {
+  // save data in the database
+  $response = "END Selected Twin VIP Toilet has been captured successfully! Thank you LWSP.";
+}
+
+
+elseif ($textArr[0] == 2 && $textArr[1] == 1 && $textArr[2] == 3 && $level == 3) {
+  $response = "CON Please enter your HH Code";
+}
+elseif ($textArr[0] == 2 && $textArr[1] == 1 && $textArr[2] == 3 && $level == 4) {
+  // save data in the database
+  $response = "END Selected Pour Flash Toilet has been captured successfully! Thank you LWSP.";
+}
+
+
+elseif ($textArr[0] == 2 && $textArr[1] == 1 && $textArr[2] == 4 && $level == 3) {
+  $response = "CON Please enter your HH Code";
+}
+elseif ($textArr[0] == 2 && $textArr[1] == 1 && $textArr[2] == 4 && $level == 4) {
+  // save data in the database
+  $response = "END Selected Septic Tank has been captured successfully! Thank you LWSP.";
+}
+
+else if ( $text == "2*2" ) {
   $response  = "CON  Technology Cost \n";
   $response .= "1. Single VIP Toilet \n";
   $response .= "2. Twin VIP Toilet \n"; 
   $response .= "3. Pour Flash Toilet \n"; 
   $response .= "4. Septic Tank \n"; 
 }
-else if ( $text == "1*2*1" ) {
-  $svipcost  .= "MK45,000.00";
+else if ( $text == "2*2*1" ) {
+  $svipcost  = "MK45,000.00";
   $response = "END Single VIP Toilet costs ".$svipcost;
+  
 }
-else if ( $text == "1*2*2" ) {
-  $tvipcost  .= "MK46,000.00";
+else if ( $text == "2*2*2" ) {
+  $tvipcost  = "MK46,000.00";
   $response = "END Twin VIP Toilet costs ".$tvipcost;
 }
-else if ( $text == "1*2*3" ) {
-  $pfcost  .= "MK47,000.00";
+else if ( $text == "2*2*3" ) {
+  $pfcost  = "MK47,000.00";
   $response = "END Pour Flash  Toilet costs ".$pfcost;
 }
-else if ( $text == "1*2*4" ) {
-  $stcost  .= "MK48,000.00";
+else if ( $text == "2*2*4" ) {
+  $stcost  = "MK48,000.00";
   $response = "END Septic Tank costs ".$stcost;
 }
 
-if ( $text == "2" ) {
+if ( $text == "3" ) {
   $response  = "CON  Payment for Service  \n";
   $response .= "1. Airtel Money \n";
   $response .= "2. Mpamba \n";
   $response .= "3. Electronic Funds Transfer \n";
   $response .= "4. Direct Bank Deposit \n";
 }
-else if ( $text == "2*1" ) {
-  $response  = "CON Payment Reference for AM \n";
-  $response .= "1. Payment Reference \n";
-}
 
-else if ( $text == "2*1*1" ) {
-  $response  = "CON  Enter Payment Reference \n";
+//Start payment recording Airtel Money
+elseif ($textArr[0] == 3 && $textArr[1] == 1  && $level == 2) {
+  $response = "CON Please enter your Household Code";
 }
+elseif ($textArr[0] == 3 && $textArr[1] == 1  && $level == 3) {
+  $response = "CON Please enter Amount Paid on Airtel Money";
+}
+elseif ($textArr[0] == 3 && $textArr[1] == 1  && $level == 4) {
+  $response = "CON Please enter Payment Reference on Airtel Money";
+}
+elseif ($textArr[0] == 3 && $textArr[1] == 1  && $level == 5) {
+  // save data in the database
+  $response = "END Payment has been captured successfully --- waiting verification! Thank you LWSP.";
+}
+//End payment recording Airtel Money
 
-else if ( $text == "2*2" ) {
-  $response  = "CON  Payment Reference for Mpamba \n";
-  $response .= "1. Payment Reference \n";
+//Start payment recording Mpamba
+elseif ($textArr[0] == 3 && $textArr[1] == 2  && $level == 2) {
+  $response = "CON Please enter your Household Code";
 }
-else if ( $text == "2*2*1" ) {
-  $response  = "CON  Enter Payment Reference \n";
+elseif ($textArr[0] == 3 && $textArr[1] == 2  && $level == 3) {
+  $response = "CON Please enter Amount Paid on Mpamba";
 }
+elseif ($textArr[0] == 3 && $textArr[1] == 2  && $level == 4) {
+  $response = "CON Please enter Payment Reference on Mpamba";
+}
+elseif ($textArr[0] == 3 && $textArr[1] == 2  && $level == 5) {
+  // save data in the database
+  $response = "END Payment has been captured successfully --- waiting verification! Thank you LWSP.";
+}
+//End payment recording Mpamba
 
-else if ( $text == "2*3" ) {
-  $response  = "CON  Payment Reference for EFT \n";
-  $response .= "1. Payment Reference \n";
+//Start payment recording EFT
+elseif ($textArr[0] == 3 && $textArr[1] == 3  && $level == 2) {
+  $response = "CON Please enter your Household Code";
 }
-else if ( $text == "2*3*1" ) {
-  $response  = "CON  Enter Payment Reference \n";
+elseif ($textArr[0] == 3 && $textArr[1] == 3  && $level == 3) {
+  $response = "CON Please enter Amount Paid on EFT";
 }
+elseif ($textArr[0] == 3 && $textArr[1] == 3  && $level == 4) {
+  $response = "CON Please enter Payment Reference on EFT";
+}
+elseif ($textArr[0] == 3 && $textArr[1] == 3  && $level == 5) {
+  // save data in the database
+  $response = "END Payment has been captured successfully --- waiting verification! Thank you LWSP.";
+}
+//End payment recording EFT
 
-else if ( $text == "2*4" ) {
-  $response  = "CON  Payment Reference for Direct Deposit \n";
-  $response .= "1. Payment Reference \n";
+//Start payment recording DBD
+elseif ($textArr[0] == 3 && $textArr[1] == 4  && $level == 2) {
+  $response = "CON Please enter your Household Code";
 }
-else if ( $text == "2*4*1" ) {
-  $response  = "CON  Enter Payment Reference \n";
+elseif ($textArr[0] == 3 && $textArr[1] == 4  && $level == 3) {
+  $response = "CON Please enter Amount Paid on Direct Bank Deposit";
 }
-else if ( $text == "3" ) {
-  $text .= "";
-  $response  = "CON  Enter Household Code \n";
+elseif ($textArr[0] == 3 && $textArr[1] == 4  && $level == 4) {
+  $response = "CON Please enter Payment Reference on Direct Bank Deposit";
 }
+elseif ($textArr[0] == 3 && $textArr[1] == 4  && $level == 5) {
+  // save data in the database
+  $response = "END Payment has been captured successfully --- waiting verification! Thank you LWSP.";
+}
+//End payment recording DBD
 
 
-//echo response
+if ($textArr[0] == 4 && $level == 1) {
+  $response = "CON Please enter your Household Code";
+}
+elseif ($textArr[0] == 4 && $level == 2) {
+  $progress = "20%";//to fetch from database
+  $response = "END Current Works Progress for Household is ".$progress;
+}
 
 header('Content-type: text/plain');
 echo $response
